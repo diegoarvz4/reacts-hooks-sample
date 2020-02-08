b7d98919a5f6a12bd303b0ea8186d9b46f0f3b80
DiegoArvz <darvz.gtz@gmail.com>
2020-02-07 21:57:50 -0600

    Hooks Basic Sample

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={(id) => {
+          console.log(id)
+          setUserIngredients(prevIg => prevIg.filter(ig => ig.id !== id));
+          }} />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
